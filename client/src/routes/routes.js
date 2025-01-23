0bc2157926e93b5059f78cc0ec34b4056bf52998
ngophuc29 <ngophuc2911@gmail.com>
2025-01-23 23:24:19 +0700

    fix UI checkout

--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -1,11 +1,16 @@
 import Home from "@/pages/Home/Home";
 import Login from "@/pages/Login/Login";
 import Signup from "@/pages/Signup/Signup";
-
+import Cart from "../pages/Cart/Cart";
+import CheckoutPage from "../pages/CheckOut/CheckoutPage";
 const publicRoutes = [
   { path: "/home", component: Home },
   { path: "/login", component: Login },
   { path: "/signup", component: Signup },
+  { path: "/cart", component: Cart },
+  { path: "/checkout", component: CheckoutPage },
+
+
 ];
 
 // Đăng nhập mới xem được (Login)
