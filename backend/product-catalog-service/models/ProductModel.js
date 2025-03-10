de2f98837ac14148fca179b17bc9f32331ebdb9d
ngophuc29 <ngophuc2911@gmail.com>
2025-03-10 13:12:05 +0700

    model product thêm details ,color chuyển thành mảng , render data theo id của product ra UI

--- backend/product-catalog-service/models/ProductModel.js
+++ backend/product-catalog-service/models/ProductModel.js
@@ -3,13 +3,15 @@ const mongoose = require("mongoose");
 const productSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    description: { type: String , required: true},
-    brand: { type: String , required: true},
+    description: { type: String, required: true },
+    details: { type: [String], required: true }, // Chuyển details thành mảng
+    brand: { type: String, required: true },
     category: { type: String, required: true },
     price: { type: Number, required: true },
     discount: { type: Number, default: 0 },
     image: { type: String },
-    color: { type: String, required: true },
+    // color: { type: String, required: true },
+    color: { type: [String], required: true }, // Mảng màu sắc
     new: { type: Boolean, default: false },
     stock: { type: Number, default: 0 },
     rating: { type: Number, default: 0 },
