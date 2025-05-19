51235da48082d813a7a4d7fd1afe048f9183b95b
ngophuc <95750619+ngophuc29@users.noreply.github.com>
2025-05-19 15:19:20 +0700

    Update images.js

--- client/src/constants/images.js
+++ client/src/constants/images.js
@@ -1,4 +1,4 @@
-import Banner from "../assets/images/Banner.png";
+import Banner from "../assets/images/banner.png";
 import CustomeBuilds from "../assets/images/CustomeBuilds.png";
 import Desktops from "../assets/images/Desktops.png";
 import GamingMonitors from "../assets/images/GamingMonitors.png";
